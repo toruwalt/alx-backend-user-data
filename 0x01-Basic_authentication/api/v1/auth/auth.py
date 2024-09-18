5007ba8c70b9438980c338963b08f199ac73ee13
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-18 14:07:33 +0100

    Basic Auth - Task 3:Attempt 6

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,14 +5,17 @@ from flask import request
 from typing import List, TypeVar
 
 
-class Auth:
+class Auth():
     """Class that authenticates"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Method to insist on Authentication"""
         return False
     
     def authorization_header(self, request=None) -> str:
+        """Method to header"""
         return None
     
-    def current_user(self, request=None):
+    def current_user(self, request=None) -> TypeVar('User'): # type: ignore
+        """Loose me"""
         return None
