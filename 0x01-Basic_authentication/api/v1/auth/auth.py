49dce9e90cd4f4d4d43d3a46ea32704ba8f01398
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-18 14:09:32 +0100

    Basic Auth - Task 3:Attempt 7

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,11 +11,11 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method to insist on Authentication"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """Method to header"""
         return None
-    
-    def current_user(self, request=None) -> TypeVar('User'): # type: ignore
+
+    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """Loose me"""
         return None
