dca0b9d49ccaca5a78341bfe0ce4ca4ed39eb9b6
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-19 04:16:04 +0100

    Basic Auth - Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Basic Auth Module"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -13,5 +14,21 @@ class BasicAuth(Auth):
                 if authorization_header.startswith('Basic '):
                     return authorization_header.removeprefix('Basic ')
                 return None
+            return None
+        return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """returns the decoded value of a Base64 string"""
+        if base64_authorization_header:
+            if isinstance(base64_authorization_header, str):
+                try:
+                    decoded_bytes = base64.b64decode(
+                        base64_authorization_header)
+                    decoded_string = decoded_bytes.decode('utf-8')
+                    return decoded_string
+                except Exception as e:
+                    return None
             return None
         return None
