01242408acd38b26543a4f734d266f35338ac8d4
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-19 03:49:19 +0100

    Basic Auth - Task 7:Attempt 2

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,17 +5,9 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Class that authenticates"""
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """returns the Base64 part of the Authorization header"""
-        """
-        Return None if authorization_header is None
-        Return None if authorization_header is not a string
-        Return None if authorization_header doesn’t start by Basic (with a space at the end)
-        Otherwise, return the value after Basic (after the space)
-        You can assume authorization_header contains only one Basic
-        
-        """
-
         if authorization_header:
             if isinstance(authorization_header, str):
                 if authorization_header.startswith('Basic '):
