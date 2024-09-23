4590b3a28f7ef5cc4e70985933b9ff61de852970
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-23 14:43:24 +0100

    User Auth Service - Task 1

---
+++ 0x03-user_authentication_service/main_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+from db import DB
+from user import User
+
+my_db = DB()
+
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print("++++++++++++++++++")
+print(user_1.id)
+print("++++++++++++++++++")
+
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print("++++++++++++++++++")
+print(user_2.id)
+print("++++++++++++++++++")
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,10 +13,5 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=False)
-    reset_token = Column(String(250), nullable=False)
-
-    def __repr__(self):
-        """String representation of an object"""
-        return "<User(name='%s', fullname='%s', nickname='%s')>" % (
-                            self.name, self.fullname, self.nickname)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
