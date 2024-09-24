3d4ca1da8d7bbbeb6982805982aae7b9f1550709
Torubiri Walter <toruwalt1997@gmail.com>
2024-09-24 13:03:14 +0100

    User Auth Service - Task 1:Attempt 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
+from typing import Tuple
 
 from user import Base
 from user import User
@@ -33,9 +35,28 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adding a new user to the database"""
-        new_user = User()
-        new_user.email = email
-        new_user.hashed_password = hashed_password
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
+
+    
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user based on a set of filters.
+        """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
